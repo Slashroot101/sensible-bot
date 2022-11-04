65196b744f8953ce6e228b14ebd256882a4e7a31
Samuel Curry <samc735@gmail.com>
2022-11-04 04:24:26 -0400

    added try catch

--- src/libs/events/interactionCreate.js
+++ src/libs/events/interactionCreate.js
@@ -1,6 +1,11 @@
+const logger = require('../logger');
 
 module.exports = async (interaction, user) => {
-    const command = interaction.client.commands.get(interaction.commandName);
+    try {
+        const command = interaction.client.commands.get(interaction.commandName);
 
-    await command.execute(interaction, user);
+        await command.execute(interaction, user);
+    } catch (err) {
+        logger.err(err, 'An error occured executing a command');
+    }
 };
